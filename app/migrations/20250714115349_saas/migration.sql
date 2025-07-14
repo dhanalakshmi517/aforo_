22186daa78906b4be7f58d2935e27fe85a25720c
eswarprasadbb <106152713+eswarprasadbb@users.noreply.github.com>
2025-07-14 17:56:31 +0530

    small change (#27)
    Co-authored-by: eswarprasadb <eswarprasad@aforo.ai>

---
+++ app/migrations/20250714115349_saas/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "productName" TEXT NOT NULL,
+    "productType" TEXT NOT NULL,
+    "billable" BOOLEAN NOT NULL,
+    "status" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
