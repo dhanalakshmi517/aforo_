e357bf4b5beb151d61bdf0efe918e8ace2e80381
dhanalakshmi517 <jinkadhanalakshmi8@gmail.com>
2025-07-15 10:01:52 +0530

    changes

--- app/src/client/components/SideNavbar/SideNavbar.tsx
+++ app/src/client/components/SideNavbar/SideNavbar.tsx
@@ -136,7 +136,7 @@ function SideNavbar({ activeTab, onTabClick, hidden }: SidebarProps): JSX.Elemen
       ),
     },
     {
-      name: 'Subscriptionss',
+      name: 'Subscriptionsss',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
           <g clipPath="url(#clip0_2599_1484)">
