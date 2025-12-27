1cf18d5bd4cd7a79298aeb141e3935849245ae83
eswarprasadb <eswarprasad@aforo.ai>
2025-12-27 17:01:10 +0530

    DP fix

--- app/src/client/components/componenetsss/Filter.tsx
+++ app/src/client/components/componenetsss/Filter.tsx
@@ -9,7 +9,7 @@ type Props = {
   onClick?: () => void;
   ariaLabel?: string;
   className?: string;
-  buttonRef?: React.RefObject<HTMLButtonElement | null>;
+  buttonRef?: React.Ref<HTMLButtonElement>;
 };
 
 const Filter: React.FC<Props> = ({
@@ -25,7 +25,7 @@ const Filter: React.FC<Props> = ({
   return (
     <Tooltip content="Filters" position="bottom">
       <button
-        ref={buttonRef as React.LegacyRef<HTMLButtonElement>}
+        ref={buttonRef}
         type="button"
         className={`filter-btn ${isActive ? "is-active" : ""} ${
           isDisabled ? "is-disabled" : ""
